9fe75dc5a8d4a32ccd88ab126f1ec6039c291c15
nawzaysfinah <nawzaysfinah@gmail.com>
2023-04-04 11:34:10 +0800

    Added reply keyboard for scheduler

--- gimmequotes_2.py
+++ gimmequotes_2.py
@@ -1,5 +1,4 @@
 import logging
-# from telegram import Update
 from telegram.ext import filters, ApplicationBuilder, MessageHandler, CommandHandler, ContextTypes
 import requests
 import notion_client
@@ -7,9 +6,10 @@ from notion_client import Client
 from datetime import datetime, timezone
 import os
 import json # to PARSE JSON string to return values
-# import random # to randomise quotes selected from database
-# import itertools
-import time #to add delay to telegram bot
+import time # to add delay to telegram bot
+import random # to randomly select quote from Notion database
+import schedule # to create a schedule of posting
+import time
 
 from telegram import __version__ as TG_VER
 from setup import GIMME, TEST, NOTION_TOKEN, DATABASE_ID
@@ -46,14 +46,10 @@ from flask import Flask, request
 app = Flask(__name__)
 
 # Declare variables
-# declared in setup.py
+# Declared in setup.py
 API_TOKEN = TEST
 NOTION_TOKEN # This is the token for your notion API
 DATABASE_ID # This is the database ID of where your quotes are stored
-
-# # Create an Updater instance using the telegram API token
-# updater = Updater(token=API_TOKEN, use_context=True)
-# dispatcher = updater.dispatcher
 
 headers = {
         "Authorization": "Bearer " + NOTION_TOKEN,
@@ -97,7 +93,7 @@ async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="If you're ready, you can start sending me quotes!")
 
 async def give(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="To send a quote: Please send them to me in the following format:?\n" 
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="To send a quote: Please send them to me in the following format:\n" 
     + "This is the quote.'/'This is the author's name'\n")
     time.sleep(2)
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Alright buddy, what do you have for me?")
@@ -106,37 +102,70 @@ async def give(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def quote(update:Update, context: ContextTypes.DEFAULT_TYPE):
 # Collect the message
     message = update.message.text
+    if len(message) > 3:
+        def create_page(data: dict):
+            create_url = "https://api.notion.com/v1/pages"
+            payload = {"parent": {"database_id" : DATABASE_ID}, "properties": data}
 
-    def create_page(data: dict):
-        create_url = "https://api.notion.com/v1/pages"
-        payload = {"parent": {"database_id" : DATABASE_ID}, "properties": data}
+            res = requests.post(create_url, headers=headers, json=payload)
+            print(str(res.status_code))
+            
+            return res
 
-        res = requests.post(create_url, headers=headers, json=payload)
-        print(str(res.status_code))
+        pear = str(message)
+        quote_in, author_in = pear.split("/")
+        print("New quote!: \n\n" + quote_in)
+        print("by " + author_in)
+
+        Author =  author_in
+        Quote = quote_in
+
+        await context.bot.send_message(chat_id=update.effective_chat.id, text='"'+ Quote + '"\n' + "- " + Author)
+        await context.bot.send_message(chat_id=update.effective_chat.id, text='Thank you for the quote!')
+        await context.bot.send_message(chat_id=update.effective_chat.id, text='/give me another quote! or /receive a quote at random from our database?')
+        published_date = datetime.now().astimezone(timezone.utc).isoformat()
+        data = {
+            "Author": {"title": [{"text": {"content": Author}}]},
+            "Quote": {"rich_text": [{"text": {"content": Quote}}]},
+            "Published": {"date": {"start": published_date, "end": None}}
+        }
+
+        create_page(data)
         
-        return res
+    else:
+        if update.message.text == "Yes":
+            
+            send_time = "09:00:00"
+            schedule.every().day.at(send_time).do(receive)
+            
+            def start_scheduler():
+                while True:
+                    schedule.run_pending()
+                    time.sleep(1)
 
-    pear = str(message)
-    quote_in, author_in = pear.split("/")
-    print("New quote!: \n\n" + quote_in)
-    print("by " + author_in)
+            start_scheduler()  
+        
+        else: 
+            await context.bot.send_message(chat_id=update.effective_chat.id, text="Message scheduler remains off")
 
-    Author =  author_in
-    Quote = quote_in
 
-    await context.bot.send_message(chat_id=update.effective_chat.id, text='"'+ Quote + '"\n' + "- " + Author)
-    await context.bot.send_message(chat_id=update.effective_chat.id, text='Thank you for the quote!')
-    await context.bot.send_message(chat_id=update.effective_chat.id, text='/give me another quote! or /receive a quote at random from our database?')
-    published_date = datetime.now().astimezone(timezone.utc).isoformat()
-    data = {
-        "Author": {"title": [{"text": {"content": Author}}]},
-        "Quote": {"rich_text": [{"text": {"content": Quote}}]},
-        "Published": {"date": {"start": published_date, "end": None}}
-    }
 
-    create_page(data)
+async def schedule(update:Update, context:ContextTypes.DEFAULT_TYPE):     
+    DECISION = range(1)
+    # Schedule a message everyday at 9:00 AM
+    # await context.bot.send_message(chat_id=update.effective_chat.id, text="Do you want to turn on the message scheduler?")
+    reply_keyboard = [["Yes", "No"]]
+    
+    await update.message.reply_text(
+        "Do you want to turn on the message scheduler?",
+        
+        reply_markup=ReplyKeyboardMarkup(
+                reply_keyboard, one_time_keyboard=True, input_field_placeholder="Yes or no?"
+        ),
+    )
+    return DECISION
 
-#need to write new command handler to return code from notion database at random with Quote & Author.
+# Command handler to return code from notion database at random with Quote & Author.
 async def receive(update:Update, context:ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text='chotomate! retrieving quote for you des...')
     
@@ -146,16 +175,6 @@ async def receive(update:Update, context:ContextTypes.DEFAULT_TYPE):
     get_all = num_pages is None
     page_size = 100 if get_all else num_pages
     payload = {"page_size": page_size}
-
-    #add a filter to return only the filtered quote called #payload_1quote, worked for awhile but stopped working
-    payload_1quote = {
-        "filter": {
-            "property": "Chosen item",
-            "checkbox": {
-                "equals": True
-            }
-        }
-    }   
 
     res = requests.request("POST",readURL,json=payload, headers=headers) #change json to filter for correct things
     data = res.json()
@@ -177,17 +196,12 @@ async def receive(update:Update, context:ContextTypes.DEFAULT_TYPE):
     for result in results:
         # print(result["properties"]["Quote"]["rich_text"][0]["plain_text"]) # this prints it all
         """
-        quotes = result["properties"]["Quote"]["rich_text"][0]["plain_text"] # this is a string, each line separated by 
+        quotes = result["properties"]["Quote"]["rich_text"][0]["plain_text"]
         author = result["properties"]["Author"]["title"][0]["plain_text"]
         """
         quotes.append(result["properties"]["Quote"]["rich_text"][0]["plain_text"])
         author.append(result["properties"]["Author"]["title"][0]["plain_text"])
-        
-        # print(quotes)
-        # print(author)
 
-    # ======== Will print the Quote at index 12 ========
-    import random
     quoteChoice = random.randint(0, len(quotes)) # pick a random number between 0 and the number of available quotes
     print(quoteChoice) # prints the selected random number
     Quote_msg = str(quotes[quoteChoice]) # picks the quote at index of the random number generated
@@ -196,13 +210,15 @@ async def receive(update:Update, context:ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=Quote_msg + "\n\n - " + Author_msg)
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Would you like to /give another quote or /receive a new one?\n" + 
                                    "Forward this quote to your friends, to share the love!")
+    
+
 
 async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I only respond to /start, /give, /receive or /help")
 
 
-#declare ApplicationBuilder & Handlers below
+# Declare ApplicationBuilder & Handlers below
 if __name__ == '__main__':
     application = ApplicationBuilder().token(API_TOKEN).build()
 
@@ -212,7 +228,9 @@ if __name__ == '__main__':
     receive_handler = CommandHandler('receive', receive)
     give_handler = CommandHandler('give', give)
     quote_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), quote)
+    schedule_handler = CommandHandler('schedule', schedule)
     unknown_handler = MessageHandler(filters.COMMAND, unknown)
+    
 
     application.add_handler(start_handler)
     application.add_handler(about_handler)
@@ -220,14 +238,16 @@ if __name__ == '__main__':
     application.add_handler(quote_handler)
     application.add_handler(receive_handler)
     application.add_handler(give_handler)
+    application.add_handler(schedule_handler)
     application.add_handler(unknown_handler)
+    
+    
 
     application.run_polling()
 
     # https://api.telegram.org/bot5912247638:AAHo1kYgW2c6TWiOuJAlWuSfKouzoVAFTQE/setWebhook?url=https://git.heroku.com/gimmequotes.git
 
-import os
-
+# WIP Webhook
 # TOKEN = os.getenv('BOTAPIKEY') # to set the telegram bot api key within codecapsules, so api key isnt visible on github
 
 # PORT = int(os.environ.get('PORT', '8443'))
